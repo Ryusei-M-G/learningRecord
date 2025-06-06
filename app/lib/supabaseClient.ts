73bc4f4ec55b49ec37c6a0326a2d1f4ac4f95b70
klran <kiran10231016@gmail.com>
2025-06-06 19:50:23 +0900

    DB接続環境変数対応

--- app/lib/supabaseClient.ts
+++ app/lib/supabaseClient.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 
 //後で環境変数に入れる
-const supabaseUrl = '';
-const supabaseAnonKey = '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_API_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import Title from "./title";
 import Button from "./button";
 import MainRecord from "./mainRecord";
-// import { supabase } from "./lib/supabaseClient";
+import { supabase } from "./lib/supabaseClient";
 
 export default async function Home() {
-  // const { data, error } = await supabase.from('test').select('*')
+  const { data, error } = await supabase.from('test').select('*')
 
-  // console.log('Server data:', data)
-  // console.log('Server error:', error)
+  console.log('Server data:', data)
+  console.log('Server error:', error)
 
 
   return (
