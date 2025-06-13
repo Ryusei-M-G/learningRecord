fa7a5fcd89a391ae6b24ca736801c593ade60f5e
Ryusei-M-G <ryusei.morishima1023@gmail.com>
2025-06-13 11:26:35 +0900

    環境変数対応

--- app/lib/supabaseClient.ts
+++ app/lib/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 
-//後で環境変数に入れる
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_API_KEY as string;
 
